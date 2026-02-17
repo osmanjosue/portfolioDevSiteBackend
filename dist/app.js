43a8ebb9574160d3179200c13f500ec2a9ee3560
osmanjosue <osmanjosue@gmail.com>
2026-02-16 22:09:10 -0600

    Se corrigio Codigo del app.js para que NGINX le asige a NODEJS cargar el backend

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,7 @@ const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
-app.use(express_1.default.static('public'));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.use(express_1.default.json());
 app.use('/api/email', require('./routes/email-routes'));
 app.get('*', (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,16 @@ import path from 'path'
 const app = express();
 app.use(cors());
 
-app.use(express.static('public'));
+// este es clave para que funcione bien con pm2
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 
 app.use('/api/email', require('./routes/email-routes'));
 
 app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+    // Codigo sugerido
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(process.env.PORT, () => {
