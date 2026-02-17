ab9897bab0c39b41b72a0d6cdd434ad25ab0490b
osmanjosue <osmanjosue@gmail.com>
2026-02-16 22:11:41 -0600

    Tercer Build para probar modificaciones en el endpoint para enviar Email

--- dist/app.js
+++ dist/app.js
@@ -9,11 +9,13 @@ const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+// este es clave para que funcione bien con pm2
 app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.use(express_1.default.json());
 app.use('/api/email', require('./routes/email-routes'));
 app.get('*', (req, res) => {
-    res.sendFile(path_1.default.resolve(__dirname, 'public/index.html'));
+    // Codigo sugerido
+    res.sendFile(path_1.default.join(__dirname, 'public', 'index.html'));
 });
 app.listen(process.env.PORT, () => {
     console.log('servidor corriendo en puerto ' + process.env.PORT);
